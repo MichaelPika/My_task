e8473353bae8bce421ca6ad21463d601a1bb0f2c
MichaelPika <xauru7@gmail.com>
2018-03-18 20:57:27 +0300

    add files

---
+++ script.js
@@ -0,0 +1,22 @@
+/* global mocha, chai, describe, it  */
+/* global isPolindrom, Hr, beforeEach */
+mocha.setup("bdd");
+const assert = chai.assert;
+
+describe("getChange", function() {
+    it("Это функция", function() {
+        return assert.isOk(typeof getChange === "function");
+    });
+
+    it ("Возврaщает массив с 6 элементами", function(){
+
+    });
+    it ("Возврaщает массив с нулевыми значениями при значeнии стоимость = оплата", function(){
+
+    });
+    it ("Возврaщает массив (значeниe стоимость !== оплата)", function(){
+
+    });
+});
+
+mocha.run();
