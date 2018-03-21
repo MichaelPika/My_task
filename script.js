f6d8f53080d8491897d678611a4ba57b789fa610
MichaelPika <xauru7@gmail.com>
2018-03-21 23:42:41 +0300

    Написаны тесты и выполнена функция getChange

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-/* global mocha, chai, describe, it  */
-/* global isPolindrom, Hr, beforeEach */
-mocha.setup("bdd");
-const assert = chai.assert;
-
-describe("getChange", function() {
-    it("Это функция", function() {
-        return assert.isOk(typeof getChange === "function");
-    });
-
-    it ("Возврaщает массив с 6 элементами", function(){
-
-    });
-    it ("Возврaщает массив с нулевыми значениями при значeнии стоимость = оплата", function(){
-
-    });
-    it ("Возврaщает массив (значeниe стоимость !== оплата)", function(){
-
-    });
-});
-
-mocha.run();
+/**
+ * Создайте функцию getChange, которая принимает 2 аргумента:
+ * первый стоимость товара и второй - оплата.
+ * Функиция рассчитывает количество сдачи, которое вы получите исходя из входных данных.
+ * Автомат может возвращать 1 коп, 5 коп, 10 коп, 20 коп, 50 коп, 1 руб.
+ * Крупные деньги должны быть первыми в вашем ответе.
+ * @param {number} cost
+ * @param {number} payment
+ * @return {object} массив денежных номиналов от  1коп. до 1р
+ */
+var getChange = function(cost, payment) {                  //cost - стоимость товара; payment - оплата.
+    var arrRest = [];
+    var i;
+    var arrNominal= [100, 50, 20, 10, 5, 1];
+    var rest = (payment * 100) - (cost * 100);
+    if (cost === payment){
+        for( i = 0; i < 6; i++){
+            arrRest.push(0);
+        }
+        return arrRest;
+    } else if (cost > payment) {
+        return ("error");
+    } else {
+        for (i = 0; i < 6; i++){
+            arrRest[i] = Math.floor(rest / arrNominal[i]);
+            rest = rest % arrNominal[i];
+        }
+    }
+    return arrRest;
+};
--- tests.js
+++ tests.js
@@ -1,22 +1,26 @@
 /* global mocha, chai, describe, it  */
-/* global isPolindrom, Hr, beforeEach */
+
 mocha.setup("bdd");
 const assert = chai.assert;
 
 describe("getChange", function() {
     it("Это функция", function() {
-        return assert.isOk(typeof getChange === "function");
+        assert.isOk(typeof getChange === "function");
     });
 
     it ("Возврaщает массив с 6 элементами", function(){
-
+        assert.lengthOf(getChange(1, 1) , 6);
     });
+
     it ("Возврaщает массив с нулевыми значениями при значeнии стоимость = оплата", function(){
-
+        assert.deepEqual(getChange(1, 1), [0, 0, 0, 0, 0, 0]);
     });
+
     it ("Возврaщает массив (значeниe стоимость !== оплата)", function(){
-
+        assert.deepEqual(getChange(1, 5), [4, 0, 0, 0, 0, 0]);
+        assert.deepEqual(getChange(3.67, 10), [6, 0, 1, 1, 0, 3]);
     });
+
 });
 
-mocha.run();# My_task
+mocha.run();
